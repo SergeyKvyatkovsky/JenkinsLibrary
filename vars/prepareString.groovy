eb009abbcb9aba984d75d88d3a0f49c5743f487e
Siarhei Kviatkouski <siarhei.kviatkouski@LeverX.local>
2022-06-27 17:37:47 +0300

    add new style

--- vars/prepareString.groovy
+++ vars/prepareString.groovy
@@ -1,5 +1,19 @@
+class Testi{
+    def inputRandomString(){
+        double rnd = Math.random()
+        print(rnd)
+        sh "echo Hello ${rnd}."
+    }
+}
+
+Testi testi = new Testi()
+testi.inputRandomString()
+
+
 def call(Map config = [:]){
     String i = ${config.name}
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}"
     return i
 }
+
+
